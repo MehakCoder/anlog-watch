7bab68606cf27c84f6967173e385b3ffac570567
MehakCoder <cmehak26@gmail.com>
2022-05-07 23:55:57 +0530

    add index

--- javascript/index.js
+++ javascript/index.js
@@ -10,10 +10,12 @@ function setClock(){
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
     setRotation(secondHand, secondsRatio)
-    setRotation(secondHand, secondsRatio)
-    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
 }
 
 function setRotation(element, rotationRatio) {
     element.style.setProperty('--rotation', rotationRatio * 360)   
 }
+
+setClock()
