64d9307e13bc160c16ca778249bc4b79113f01de
MehakCoder <cmehak26@gmail.com>
2022-05-07 23:29:29 +0530

    add index

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ setInterval(setClock, 1000)
 
 function setClock(){
     const currentDate = new Date()
-    const SecondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = currentDate.getMinutes() / 60
-    const secondsRatio = currentDate.getHours() / 60
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = currentDate.getHours() / 60
 }
