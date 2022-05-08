d665c428c5f687b8d233a3d93fffd19af393efcb
MehakCoder <cmehak26@gmail.com>
2022-05-08 17:53:52 +0530

    add javascript

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 setInterval(setClock, 1000)
 
-const hourHand = document.querySelector('[data-hour-hand]')
-const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
+const hourhand = document.querySelector('[data-hour-hand]')
+const minutehand = document.querySelector('[data-minute-hand]')
+const secondhand = document.querySelector('[data-second-hand]')
 
-function setClock(){
+function setClock()  {
     const currentDate = new Date()
     const secondsRatio = currentDate.getSeconds() / 60
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-    setRotation(secondHand, secondsRatio)
-    setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand, hoursRatio)
+    setRotation(secondhand, secondsRatio)
+    setRotation(minutehand, minutesRatio)
+    setRotation(hourhand, hoursRatio)
 }
 
 function setRotation(element, rotationRatio) {
-  element.style.setProperty('--rotation', rotationRatio * 360)
+  element.style.setProperty('--rotation' , rotationRatio * 360)
 }
 
-setClock();
+setClock()
